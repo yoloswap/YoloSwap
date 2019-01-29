68e8e9180b4c50f18c833aa9a59c92b3767eead3
trungnx <trung@kyber.network>
2019-01-29 13:28:30 +0700

    Change text from "mined" to "accepted"

--- src/components/swap/SwapView.js
+++ src/components/swap/SwapView.js
@@ -89,7 +89,7 @@ export default class SwapView extends Component {
         )}
 
         {this.props.tx.id && (
-          <div className={"swap__text common__fade-in"}>Successfully! The <a rel="noopener noreferrer" href={`${TX_URL}${this.props.tx.id}`} target="_blank">transaction</a> is mined</div>
+          <div className={"swap__text common__fade-in"}>Successfully! The <a rel="noopener noreferrer" href={`${TX_URL}${this.props.tx.id}`} target="_blank">transaction</a> is accepted</div>
         )}
 
         {!isButtonHidden && (
