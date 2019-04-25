fc6598654df44db4113018841050e39867df0490
trungnx <trung@kyber.network>
2019-04-25 13:18:11 +0700

    Add big number

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,10 @@
+import BigNumber from "bignumber.js";
+
 export function formatAmount(amount, precision = 6) {
   if (amount === undefined) return;
 
-  const amountString = amount.toFixed(18).toString();
+  const amountBigNumnber = new BigNumber(amount);
+  const amountString = amountBigNumnber.toFixed().toString();
   const indexOfDecimal = amountString.indexOf('.');
 
   return indexOfDecimal !== -1 ? parseFloat(amountString.slice(0, indexOfDecimal + (precision + 1))) : parseFloat(amountString);
