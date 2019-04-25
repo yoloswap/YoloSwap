94467ee4d784600e4a267ed35d8468a5ebb749f0
trungnx <trung@kyber.network>
2019-04-25 11:34:56 +0700

    Fix too small source amount

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,8 @@
 export function formatAmount(amount, precision = 6) {
   if (amount === undefined) return;
 
-  const amountString = amount.toString();
+  const amountString = amount.toFixed(18).toString();
+  const indexOfDecimal = amountString.indexOf('.');
 
-  return parseFloat(amountString.slice(0, (amountString.indexOf('.')) + (precision + 1)));
+  return indexOfDecimal !== -1 ? parseFloat(amountString.slice(0, indexOfDecimal + (precision + 1))) : parseFloat(amountString);
 }
