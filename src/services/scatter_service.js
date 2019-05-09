be66840657761aac8406859b9f06faef4b2ec98c
trungnx <trung@kyber.network>
2019-05-09 10:14:07 +0700

    Debug codes for EOSLynx

--- src/services/scatter_service.js
+++ src/services/scatter_service.js
@@ -22,6 +22,11 @@ export async function connect(firstTimeConnect = false) {
 
   const scatter = scatterJs.scatter;
   const identity = await scatter.getIdentity(requiredFields);
+  alert(identity.accounts[0].authority);
+  alert(identity.accounts[0].publicKey);
+  alert(identity.accounts[0].chainId);
+  alert(identity.accounts[0].name);
+  alert(identity.publicKey);
   const account = identity.accounts.find(x => x.blockchain === 'eos');
   const eos = getEosInstance(scatter, network);
 
