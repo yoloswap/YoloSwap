c96c61c48c161a17b4b8ac1c7c49558469675102
trungnx <trung@kyber.network>
2019-06-06 14:17:38 +0700

    Restore backup nodes

--- src/config/envs/staging.js
+++ src/config/envs/staging.js
@@ -4,9 +4,9 @@ const envConfig = {
   NETWORK_PORT: '443',
   NETWORK_ACCOUNT: 'yolonetwork1',
   NETWORK_HOSTS: [
-    'user-api.eoseoul.iooo',
-    'mainnet.eoscanadaaaa.com',
-    'api.eossweden.seee',
+    'user-api.eoseoul.io',
+    'mainnet.eoscanada.com',
+    'api.eossweden.se',
   ],
   NETWORK_CHAIN_ID: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   TX_URL: 'https://bloks.io/transaction/',
