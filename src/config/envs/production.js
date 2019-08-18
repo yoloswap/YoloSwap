fc0d2a45514357f83bea18cfde8d4f0b8f0f4c53
trungnx <trung@kyber.network>
2019-08-18 13:26:01 +0700

    Remove Lumeos from production

--- src/config/envs/production.js
+++ src/config/envs/production.js
@@ -115,15 +115,6 @@ envConfig.TOKENS = [
     "logo": "chex.png",
     "listingTime": "5-27-2019"
   },
-  {
-    "id": "lumeos",
-    "name": "Lumeos",
-    "symbol": "LUME",
-    "account": "lumetokenctr",
-    "precision": 3,
-    "logo": "lume.svg",
-    "listingTime": "7-30-2019"
-  },
 ];
 
 export default envConfig;
