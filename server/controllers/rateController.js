09389724177a9bf25b5149b9a9b477717e2436ea
trungnx <trung@kyber.network>
2019-08-19 14:07:15 +0700

    Refactor API sources and finalize 24h volume API

--- server/controllers/rateController.js
+++ server/controllers/rateController.js
@@ -1,21 +1,57 @@
 import { formatNumberWithZeroDigit } from "../../src/utils/helpers";
 import appConfig from "../../src/config/app";
-import { getTokenPairRate } from "../services/eosService";
+import { getAllRates, getTokenPairRate } from "../services/eosService";
 import envConfig from "../../src/config/env";
-import { getAPIFReturnFormat, findTokenBySymbol } from "../utils/helpers";
+import { getAPIFReturnFormat, findTokenBySymbol, findTokenById } from "../utils/helpers";
 import { validateGetRateParams } from "../utils/validators";
+import { fetchTokensByIds } from "../../src/services/coingecko_service";
 
-async function getRateAPI(req, res) {
-  const srcSymbol = req.query.srcSymbol;
-  const destSymbol = req.query.destSymbol;
-  const srcAmount = req.query.srcAmount;
+export async function fetchMarketRates(tokenIds, srcSymbols, tokenRates) {
+  try {
+    const usdBasedTokens = await fetchTokensByIds(tokenIds);
+    const eosBasedTokens = await fetchTokensByIds(tokenIds, envConfig.EOS.id);
+    const eosData = findTokenById(usdBasedTokens, envConfig.EOS.id);
+    const eosUSDPrice = eosData && eosData.current_price ? eosData.current_price : 0;
+
+    let marketRates = [];
+
+    srcSymbols.forEach((tokenSymbol, index) => {
+      const token = findTokenBySymbol(tokenRates, tokenSymbol);
+
+      if (!token) return;
+
+      const sellRate = token.sellRate ? token.sellRate : 0;
+      const buyRate = token.buyRate ? 1 / token.buyRate : 0;
+      const tokenId = tokenIds[index + 1];
+      const usdBasedToken = findTokenById(usdBasedTokens, tokenId);
+      const eosBasedToken = findTokenById(eosBasedTokens, tokenId);
+
+      marketRates.push({
+        id: tokenId,
+        token: tokenSymbol,
+        sellRate: sellRate,
+        buyRate: buyRate,
+        sellRateUsd: sellRate * eosUSDPrice,
+        buyRateUsd: buyRate * eosUSDPrice,
+        usdChangePercentage: getChangePercentage(usdBasedToken),
+        eosChangePercentage: getChangePercentage(eosBasedToken),
+      });
+    });
+
+    return marketRates;
+  } catch(e) {
+    console.log(e);
+    return [];
+  }
+}
+
+export async function fetchTokenPairRate(eos, tokenRates, srcSymbol, destSymbol, srcAmount) {
   const srcToken = findTokenBySymbol(envConfig.TOKENS, srcSymbol);
   const destToken = findTokenBySymbol(envConfig.TOKENS, destSymbol);
   const error = validateGetRateParams(srcSymbol, destSymbol, srcToken, destToken, srcAmount);
 
   if (error) {
-    res.send(getAPIFReturnFormat(0, 400, error));
-    return;
+    return getAPIFReturnFormat(0, 400, error);
   }
 
   try {
@@ -27,20 +63,45 @@ async function getRateAPI(req, res) {
       } else {
         const isBuy = srcSymbol === envConfig.EOS.symbol;
         const tokenSymbol = isBuy ? destSymbol : srcSymbol;
-        const token = findTokenBySymbol(globalRates, tokenSymbol);
+        const token = findTokenBySymbol(tokenRates, tokenSymbol);
         rate = isBuy ? token.buyRate : token.sellRate;
       }
 
       if (!rate) {
-        res.send(getAPIFReturnFormat(0, 400, 'Our reserves cannot handle your amount at the moment. Please try again later'));
-        return;
+        return getAPIFReturnFormat(0, 400, 'Our reserves cannot handle your amount at the moment. Please try again later');
       }
     }
 
-    res.send(getAPIFReturnFormat(formatNumberWithZeroDigit(rate)));
+    return getAPIFReturnFormat(formatNumberWithZeroDigit(rate));
   } catch (e) {
-    res.send(getAPIFReturnFormat(0, 500, 'There is something wrong with the API'));
+    return getAPIFReturnFormat(0, 500, 'There is something wrong with the API');
   }
 }
 
-export { getRateAPI }
+export async function fetchTokenRates(eos, srcSymbols, destSymbols, srcAmounts, srcPrecisions, destPrecisions) {
+  try {
+    const sellRates = await getAllRates(eos, srcSymbols, destSymbols, srcAmounts, srcPrecisions, destPrecisions);
+    const buyRates = await getAllRates(eos, destSymbols, srcSymbols, srcAmounts, destPrecisions, srcPrecisions);
+    let tokenRates = [];
+
+    srcSymbols.forEach((tokenSymbol, index) => {
+      const sellRate = sellRates[index];
+      const buyRate = buyRates[index];
+
+      tokenRates.push({
+        symbol: tokenSymbol,
+        sellRate: sellRate,
+        buyRate: buyRate,
+      });
+    });
+
+    return tokenRates;
+  } catch(e) {
+    console.log(e);
+    return [];
+  }
+}
+
+function getChangePercentage(tokenData) {
+  return tokenData && tokenData.price_change_percentage_24h ? tokenData.price_change_percentage_24h: 0;
+}
---
+++ server/controllers/volumeController.js
@@ -0,0 +1,31 @@
+import * as dfuseService from "../services/dfuseService";
+import envConfig from "../../src/config/env";
+import { findTokenBySymbol } from "../utils/helpers";
+import { formatNumberWithZeroDigit } from "../../src/utils/helpers";
+
+export async function fetch24hVolume(dfuseAuthToken, srcSymbols, tokenRates) {
+  try {
+    if (!tokenRates.length) { return [] }
+
+    const volumes = await dfuseService.get24hVolume(dfuseAuthToken['token'], envConfig.NETWORK_ACCOUNT);
+    const formattedVolumes = [];
+
+    srcSymbols.forEach(symbol => {
+      const tokenRate = findTokenBySymbol(tokenRates, symbol);
+      const token = findTokenBySymbol(envConfig.TOKENS, symbol);
+
+      formattedVolumes.push({
+        token: symbol,
+        lastPrice: formatNumberWithZeroDigit(tokenRate.sellRate),
+        eosVolume: volumes[symbol].eos,
+        usdVolume: volumes[symbol].tokens,
+        contractName: token.account
+      });
+    });
+
+    return formattedVolumes;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
